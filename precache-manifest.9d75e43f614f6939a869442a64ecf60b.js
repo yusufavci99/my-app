41191bccacd2dabc53abbbfad5528e84e4bfc3d4
yusufavci99 <yusufavci99@gmail.com>
2020-10-16 17:06:37 +0300

    Updates

---
+++ precache-manifest.9d75e43f614f6939a869442a64ecf60b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae7f4984e66c506f79c62d57c75e398",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "228660a8fd26f7e3aa1e",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/my-app/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-app/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228660a8fd26f7e3aa1e",
+    "url": "/my-app/static/js/main.61bdfae7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
